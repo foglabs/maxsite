6b4476e50251ac8380aa578530d4be3100b46331
fog <foglaboratories@gmail.com>
2015-03-18 13:28:46 -0400

    added new temp

--- app/controllers/comics_controller.rb
+++ app/controllers/comics_controller.rb
@@ -13,11 +13,23 @@ class ComicsController < ApplicationController
 	end
 
 	def create
-		Comic.create(comic_params)
+		@comic = Comic.create(comic_params)
+		@news = Newsie.last(10)
+
+		if @comic.save
+			redirect_to comic_path(@comic)
+		else
+			render 'new'
+		end
+	end
+
+	def new
+		@news = Newsie.last(10)
+		@comic = Comic.new
 	end
 
 	private
 		def comic_params
-			params.require(:comic).permit(:title, :desc, :img)
+			params.require(:comic).permit(:title, :desc, :img, :position)
 		end
 end
--- app/models/comic.rb
+++ app/models/comic.rb
@@ -3,12 +3,40 @@ class Comic < ActiveRecord::Base
 	validates :position, uniqueness: true
 
 	def nextcom
-		c = Comic.where( position: self.position + 1 ).first.try(:id)
-		c ? c : Comic.last.id
+		# c = Comic.where( position: self.position + 1 ).first.try(:id)
+		# c ? c : Comic.last.id
+
+		c = nil
+		trying = self.position + 1
+
+		# in case the order is fucked up
+		unless c
+			c = Comic.where( position: trying ).first.try(:id)
+			trying += 1
+		else
+			c = Comic.last
+		end
+
+		c
 	end
 
 	def lastcom
-		c = Comic.where( position: self.position - 1 ).first.try(:id)
-		c ? c : Comic.last.id
+		# c = Comic.where( position: self.position - 1 ).first.try(:id)
+		# c ? c : Comic.last.id
+
+		c = nil
+		trying = self.position - 1
+
+		# in case the order is fucked up
+		unless c || trying < 1
+			c = Comic.where( position: trying ).first.try(:id)
+			trying -= 1
+		end
+
+		if trying < 1
+			c = Comic.last
+		end
+		
+		c
 	end
 end
