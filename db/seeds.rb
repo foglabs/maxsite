42183c56c534ea07fbadf10db2245ed09b1f7d1c
fog <foglaboratories@gmail.com>
2015-03-29 01:05:50 -0400

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Comic.create(img: '/assets/clouds.jpg', desc: 'This is A crazy Comic!', title: 'Check me Out!', position: 1)
+Comic.create(img: '/assets/clouds.jpg', desc: 'Comic Number Two!', title: 'Same as the first', position: 2)
+Comic.create(img: '/assets/clouds.jpg', desc: 'Patrick Swayze', title: 'Checking it out', position: 3)
+
