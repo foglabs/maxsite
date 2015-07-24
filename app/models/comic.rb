0da8ee7f4a02fa089240965d1078de6489c74206
fog <foglaboratories@gmail.com>
2015-07-24 19:57:12 -0400

    fixed layout

--- app/models/comic.rb
+++ app/models/comic.rb
@@ -6,6 +6,10 @@ class Comic < ActiveRecord::Base
   has_attached_file :image, default_url: "/assets/comics/comic1.jpg", storage: :s3, path: "comics/:title.:extension", s3_credentials: { bucket: "mayhemchampion", access_key_id: ENV["AWSKEY"], secret_access_key: ENV["AWSECRET"] }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
+	def format_time
+		created_at.strftime('%b-%d-%Y')
+	end
+
 	def nextcom
 		Comic.where('position > ?', position).try(:first)
 	end
